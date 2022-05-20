cb10c1242643c5ed1668e8901fd7b8ab19f82754
tri/KUMA <trikuma@hotmail.com>
2022-05-20 21:04:13 +0800

    Added user authentication

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,17 +1,37 @@
 import "./styles/Login.css";
 import { AiFillGoogleCircle } from "react-icons/ai";
 import { FaFacebook } from "react-icons/fa";
+import {
+  Auth,
+  GoogleAuthProvider,
+  FacebookAuthProvider,
+  signInWithRedirect,
+} from "firebase/auth";
 
-function Login() {
+interface LoginProps {
+  auth: Auth;
+}
+
+function Login(props: LoginProps) {
   return (
     <div className="login-page">
       <div className="login-card">
         <h2>Welcome to Unichat!</h2>
 
-        <button className="login-button google">
+        <button
+          className="login-button google"
+          onClick={() => {
+            signInWithRedirect(props.auth, new GoogleAuthProvider());
+          }}
+        >
           <AiFillGoogleCircle /> Sign In with Google
         </button>
-        <button className="login-button facebook">
+        <button
+          className="login-button facebook"
+          onClick={() => {
+            signInWithRedirect(props.auth, new FacebookAuthProvider());
+          }}
+        >
           <FaFacebook /> Sign In with Facebook
         </button>
       </div>
